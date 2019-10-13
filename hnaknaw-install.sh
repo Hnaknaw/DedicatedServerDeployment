931a380b9145879f706f3882ff460fea23148ba6
Austin <austin@hnaknaw.com>
2019-10-12 20:25:17 -0500

    Update hnaknaw-install.sh

--- hnaknaw-install.sh
+++ hnaknaw-install.sh
@@ -22,8 +22,6 @@ else
 fi
 done
 
-
-
 #Open Game Panel Installer
 shouldloop2=true;
 while $shouldloop2; do
@@ -31,8 +29,17 @@ read -p "Would you like to launch OpenGamePanel Auto Installer? [y/n]: " delconf
 shouldloop2=false;
 if [ $delconf == 'y' ]; then
    echo "OpenGamePanel Auto Installer Initiated."
+   echo "Please enter root password"
    su - root
-   
+   apt-get install sudo libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs subversion pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl libc6-i386 lib32gcc1 libhttp-daemon-perl libarchive-extract-perl -y
+   adduser ogpagent
+   usermod -aG sudo "ogpagent"
+   su - ogpagent
+   sudo wget https://github.com/OpenGamePanel/OGP-Agent-Linux/archive/master.zip
+   sudo unzip master.zip
+   sudo ./install.sh
+
+
 elif [ $delconf == 'n' ]; then
    echo "OpenGamePanel will not be installed."
 else
